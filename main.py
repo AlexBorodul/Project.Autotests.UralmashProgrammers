7fb68d3ce8beebd185ee95f2467670ffc8887258
Aleksey Borodulin <alyosha.borodulin@ya.ru>
2024-11-14 18:22:40 +0500

    Привязка модели через GigaChain

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello Alpha")
+from langchain.chat_models.gigachat import GigaChat
+from langchain_core.messages import HumanMessage, SystemMessage
+
+chat = GigaChat(credentials="YjU2OTAwMDgtNjdlMy00ODhlLWFkMTQtYjQwN2Y1YmE1YTJhOjY1M2ZlMGZjLTk4NzEtNDEyNC1iMmFkLWYzMDM1OWFmYzhlMA==", streaming=True)
+messages = [
+    SystemMessage(
+        content="Ты программист-тестировщик, которому нужно написать автотесты для приложений банка."
+    )
+]
+
+while True:
+    user_input = input("Пользователь: ")
+    if user_input == "пока": break
+    messages.append(HumanMessage(content=user_input))
+    res = chat.invoke(messages)
+    messages.append(res)
+    print("GigaChat: ", res.content)
